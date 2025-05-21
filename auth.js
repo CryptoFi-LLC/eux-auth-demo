ad48f4431829a622b2301d0d199f5da1f181b559
Doug Whitney <doug.whitney@cryptofi.tech>
2025-05-20 19:03:50 -0600

    New files

---
+++ auth.js
@@ -0,0 +1,52 @@
+window.onload = async function () {
+  const dbpAuthEndpoint = "https://api.cryptofi-dev.com/v2/dbp/auth";
+  const urlParams = new URLSearchParams(window.location.search);
+  const hasCode = urlParams.has("code");
+  const delay = 2000; // add a small delay to allow reading the logs before redirect
+
+  try {
+    if (!hasCode) {
+      log("ℹ️ No code parameter found in URL, initiating auth flow...", null);
+      log("➡️ Sending GET request to /dbp/auth...", null);
+
+      const response = await fetch(dbpAuthEndpoint, {
+        method: "GET",
+      });
+
+      const data = await response.json();
+
+      // TODO check if PKCE is required
+      const redirectUrl = new URL(data.redirect_url);
+      log("ℹ️ Redirecting to auth URL...", redirectUrl.toString());
+
+      setTimeout(() => {
+        // redirect to auth URL
+        window.location.replace(redirectUrl.toString());
+      }, delay);
+    }
+
+    if (hasCode) {
+      const code = urlParams.get("code");
+      log("ℹ️ Code parameter found:", code);
+
+      const requestBody = {
+        code: code,
+      };
+
+      log("➡️ Sending POST request to /dbp/auth:", requestBody);
+      
+      const response = await fetch(dbpAuthEndpoint, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(requestBody),
+      });
+
+      const data = await response.json();
+      log("✅ Auth successful:", data);
+    }
+  } catch (error) {
+    log("❌ Auth error:", error.message);
+  }
+};
---
+++ logger.js
@@ -0,0 +1,27 @@
+document.getElementById("reloadButton").addEventListener("click", function () {
+  window.location.replace("/");
+});
+
+const outputElement = document.getElementById("output");
+
+function log(message, data) {
+  const logEntry = document.createElement("div");
+  logEntry.classList.add("log-entry");
+
+  const timestamp = new Date().toLocaleTimeString();
+  const formattedMessage = `[${timestamp}] ${message}`;
+
+  if (data) {
+    logEntry.innerHTML = `<p>${formattedMessage}</p><pre>${JSON.stringify(
+      data,
+      null,
+      2
+    )}</pre>`;
+  } else {
+    logEntry.innerHTML = `<p>${formattedMessage}</p>`;
+  }
+
+  outputElement.appendChild(logEntry);
+  // Auto-scroll to the bottom
+  outputElement.scrollTop = outputElement.scrollHeight;
+}
